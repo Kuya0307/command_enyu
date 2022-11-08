3f9e30f34b4ada96411437326422ef021dbd5c6c
kuya <k.oda.sys22@morijyobi.ac.jp>
2022-11-08 09:56:07 +0900

    BMIvZðñ񕶌vZł«é悤ɂµ½

--- Main.java
+++ Main.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        for(int i = 0 ;i < 2;i++){
         double  sin = scan.nextDouble();
         double tai = scan.nextDouble();
         double sin2 = sin /100; 
         double bmi = tai / (sin2 * sin2);
         System.out.println(String.format("%.2f", bmi));
+        }
     }
 }
